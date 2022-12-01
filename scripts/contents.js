4dfd059a5adaee58ac0c832c0e5f9124e7ad7643
Abobora <lucasclucas7@gmail.com>
2022-12-01 19:47:13 -0300

    Curso Web-Moderno

--- scripts/contents.js
+++ scripts/contents.js
@@ -9,18 +9,3 @@ document.querySelector('#senha').oninput = e => {
     document.querySelector('#home').style.display = 'block'
     document.querySelector('#validation').style.display = 'none'
 }
-
-document.querySelector('#postar').onclick = e => {
-    const formData = new FormData()
-    const xhr = new XMLHttpRequest()
-    const foto = document.querySelector('#selectedFile')
-    console.log(foto.files[0])
-    formData.append(foto.name, foto.files[0])
-
-
-
-    fetch('/addPost', {
-        method: 'POST',
-        body: formData
-    },).then(r => console.log(r))
-}
--- server.js
+++ server.js
@@ -1,16 +1,64 @@
-const express = require('express')
-const upload = require('express-fileupload')
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const _ = require('lodash');
 
-const app = express()
+const app = express();
 
+// enable files upload
+app.use(fileUpload({
+    createParentPath: true
+}));
+
+//add other middleware
+app.use(cors());
 app.use(express.static('.'))
-app.use(upload())
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(morgan('dev'));
 
-app.post('/sendPost', (req, res) => {
-    if(req.files) {
-        console.log(req.files)
-        res.send('Arquivo enviado')
+//start app 
+const port = process.env.PORT || 3000;
+
+app.post('/upload', async (req, res) => {
+    try {
+        if(!req.files) {
+            res.send({
+                status: false,
+                message: 'No file uploaded'
+            });
+        } else {
+            let data = []; 
+    
+            //loop all files
+            _.forEach(_.keysIn(req.files.photos), (key) => {
+                let photo = req.files.photos[key];
+                
+                //move photo to uploads directory
+                photo.mv('./uploads/' + photo.name);
+
+                //push file details
+                data.push({
+                    name: photo.name,
+                    mimetype: photo.mimetype,
+                    size: photo.size
+                });
+            });
+    
+            //return response
+            res.send({
+                status: true,
+                message: 'Files are uploaded',
+                data: data
+            });
+        }
+    } catch (err) {
+        res.status(500).send(err);
     }
-})
+});
 
-app.listen(5000, () => console.log('Server Ativo!'))
+app.listen(port, () => 
+  console.log(`App is listening on port ${port}.`)
+);
